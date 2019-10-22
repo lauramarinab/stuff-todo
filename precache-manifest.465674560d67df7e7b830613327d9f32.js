23604b1a73f5c2437709523d3dd89023de6079f9
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2019-10-22 17:56:22 +0200

    Updates

---
+++ precache-manifest.465674560d67df7e7b830613327d9f32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c183d87ab583107667dc9f76d64ef8c9",
+    "url": "/todolist-react-redux/index.html"
+  },
+  {
+    "revision": "067d8e14878f4ebf70a7",
+    "url": "/todolist-react-redux/static/css/main.656e60f3.chunk.css"
+  },
+  {
+    "revision": "c718d3b043918d1dddfd",
+    "url": "/todolist-react-redux/static/js/2.4f30f65d.chunk.js"
+  },
+  {
+    "revision": "067d8e14878f4ebf70a7",
+    "url": "/todolist-react-redux/static/js/main.8dd20042.chunk.js"
+  },
+  {
+    "revision": "03f84244d1d93f4ef4d6",
+    "url": "/todolist-react-redux/static/js/runtime-main.e1fdc228.js"
+  },
+  {
+    "revision": "593ef17415c7a48167d0e123f84dd46b",
+    "url": "/todolist-react-redux/static/media/checked-pallino.593ef174.svg"
+  },
+  {
+    "revision": "3f3ef3a272e6867d97c476fa661b1504",
+    "url": "/todolist-react-redux/static/media/pallino.3f3ef3a2.svg"
+  },
+  {
+    "revision": "509ee1f49eb208a95c8e086ca2bd3a8c",
+    "url": "/todolist-react-redux/static/media/plus.509ee1f4.svg"
+  },
+  {
+    "revision": "7e444d02af29ff246eb26252ff439c45",
+    "url": "/todolist-react-redux/static/media/remove.7e444d02.svg"
+  }
+]);
