fbf2b3ccde83265dd52d3cd78ef9fa1293d127a2
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2019-12-01 12:44:49 +0100

    removed inputContainer and update Input, added onCreateTodo with client.post

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import useSWR from "swr";
 import { fetch } from "src/client";
 import { TodoT } from "src/types/Todo";
-import { InputContainer } from "./containers/InputContainer";
 import { HelloLaura } from "./components/HelloLaura";
 import { List } from "./components/ListTodo/List";
 import { AnalysisTodo } from "./components/AnalysisTodo";
 import client from "src/client";
+import { Input } from "./components/Input/Input";
 
 const App: React.FC = () => {
   const { data, error } = useSWR<Array<TodoT>>("/todo", fetch);
@@ -17,6 +17,10 @@ const App: React.FC = () => {
 
   const onComplete = async (id: string, completed: boolean) => {
     await client.patch(`todo/${id}`, { completed });
+  };
+
+  const onCreateTodo = async (description: string) => {
+    await client.post("todo/", { description });
   };
 
   if (error) {
@@ -31,7 +35,7 @@ const App: React.FC = () => {
     <>
       <HelloLaura />
       <AnalysisTodo todos={data} />
-      <InputContainer />
+      <Input onCreateTodo={onCreateTodo} />
       <List todos={data} onComplete={onComplete} onTrash={onTrash} />
     </>
   );
